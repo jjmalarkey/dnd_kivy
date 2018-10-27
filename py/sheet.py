7744ee6005fe0ff89d7f700d046ff4651dc4b7de
jjmalarkey <38889395+jjmalarkey@users.noreply.github.com>
2018-10-27 11:56:47 -0700

    Begin class writing

---
+++ py/sheet.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+class CharacterSheetClass:
+    def __init__(self,name,race,classClass):
+        self.name = name
+        self.race = race
+        self.classClass = classClass
+
+
+    #Class classes
+
+class BaseClassClass:
+
+class BarbarianClassClass(BaseClassClass):
+
+class BardClassClass(BaseClassClass):
+
+class ClericClassClass(BaseClassClass):
+
+class DruidClassClass(BaseClassClass):
+
+class FighterClassClass(BaseClassClass):
+
+class MonkClassClass(BaseClassClass):
+
+class PaladinClassClass(BaseClassClass):
+
+class RogueClassClass(BaseClassClass:)
+
+class RangerClassClass(BaseClassClass):
+
+class SorcererClassClass(BaseClassClass):
+
+class WarlockClassClass(BaseClassClass):
+
+class WizardClassClass(BaseClassClass):
+
+    #Modifiers classes
+
+class BaseMod:
+    def __init__(self, value, mod_type):
+        self.value = value
+        self.mod_type = mod_type
+
+class StaticMod(BaseMod):
+    def __init__(self, value, mod_type):
+        self.value = value
+        self.mod_type = mod_type
+
+class DynamicMod(BaseMod):
+    def __init__(self, value, mod_type):
+        self.value = value
+        self.mod_type = mod_type
+
+class SituationalMod(BaseMod):
+    def __init__(self, value, mod_type):
+        self.value = value
+        self.mod_type = mod_type
+
